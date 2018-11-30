d1c270b29890de7fbf262a096bafe2411b0dc548
BurdenKing <lzheng20@my.bcit.ca>
2018-11-29 21:17:40 -0800

    link create event to firebase

--- js/createEvent.js
+++ js/createEvent.js
@@ -1,14 +1,17 @@
+
+
 var root = firebase.database().ref().child('events');
 
-$('#myBtn').click(function(){
+$('#button3').click(function(){
   var myEventName = $('#eventName').val();
   var myLocation = $('#location').val();
   var myTime = $('#time').val();
   var myDate = $('#date').val();
-  var myDescription = $('#desciption').val();
+  var myDescription = $('#description').val();
+    console.log(myEventName);
     
   root.push().set({
-    Event_Name: myEventName ,
+    Event_Name: myEventName,
     Location: myLocation,
     Time: myTime,
     Date: myDate,
