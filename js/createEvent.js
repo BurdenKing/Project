30ea75aa52bf627f10d9b1986e2fede0412fc50e
BurdenKing <lzheng20@my.bcit.ca>
2018-11-30 03:28:12 -0800

    Create skeleton for upcomingEvent.html and pastEvent.html, updates createEvent.html

--- js/createEvent.js
+++ js/createEvent.js
@@ -2,19 +2,17 @@
 
 var root = firebase.database().ref().child('events');
 
-$('#button3').click(function(){
+$('#createButton').click(function(){
   var myEventName = $('#eventName').val();
   var myLocation = $('#location').val();
-  var myTime = $('#time').val();
-  var myDate = $('#date').val();
+  var myDateAndTime = $('#dateAndTime').val();
   var myDescription = $('#description').val();
     console.log(myEventName);
     
   root.push().set({
     Event_Name: myEventName,
     Location: myLocation,
-    Time: myTime,
-    Date: myDate,
+    DateAndTime : myDateAndTime,
     Description: myDescription
   })
   alert("Event successfully created!");
