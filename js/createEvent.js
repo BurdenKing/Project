4c5d0c432728e888863a20d2ba18096c957a6ca4
BurdenKing <lzheng20@my.bcit.ca>
2018-12-02 22:19:58 -0800

    remove lib, update createEvent with groupMembers linked, update upcoming events and past events with checking if user is valid.

--- js/createEvent.js
+++ js/createEvent.js
@@ -7,21 +7,24 @@ var groupRef = firebase.database().ref().child('groups');
 var groupNameArray =[];
 
 
-   
+
+
 
 
 
 $('#createButton').click(function(){
-    
-  
+
+
   
 groupRef.on('value', function(snapshot) {
+    var groupMembersArray = [];
     snapshot.forEach(function(childSnapshot) {
+        console.log(childSnapshot.val());
       var childData = childSnapshot.val().groupName;
       groupNameArray.push(childData);
     });
+    
 
-});
 
     function isValidGroups(myArray){
         var tempCount = 0;
@@ -37,6 +40,7 @@ groupRef.on('value', function(snapshot) {
         }
     }
     
+
   
 
   var myEventName = $('#eventName').val();
@@ -47,25 +51,42 @@ groupRef.on('value', function(snapshot) {
   var isValidGroup = false;
   var temp = myGroup.split(",");
 
-
-    
- for(var i = 0; i < temp.length; i++){
-     var j = temp[i];
-     if (isValidGroups(temp)){
+  groupRef.on('value', getAllMembers);
+    function getAllMembers(log){
+        var groups = log.val();
+        var groupArray = Object.keys(groups);
+    for(var i = 0; i < groupArray.length; i++){
+        var k = groupArray[i];
+        var myGroupMembers = groups[k].groupMembers;
+        var myGroupName = groups[k].groupName;
+        console.log(myGroupName);
+        
+        for(var j = 0; j <  temp.length; j++){
+            console.log(temp);
+            if(temp[j] == myGroupName){
+                groupMembersArray.push(myGroupMembers);
+            }
+        }
+    }
+}
+  
+    if (isValidGroups(temp)){
            root.push().set({
                 Event_Name: myEventName,
                 Location: myLocation,
                 DateAndTime : myDateAndTime,
                 Description: myDescription,
                 Group: myGroup,
-                VoteCount : 0
+                VoteCount : 0,
+                AllMembers : groupMembersArray
   })
+
   alert("Event successfully created!");
      } else {
          alert("Cannot find group(s)! Please check and invite group(s) again!");
      }
- }
 });
+    });
          
          
 
